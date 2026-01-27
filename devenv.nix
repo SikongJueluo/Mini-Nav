f9a359fc2825c6284c63938928c9f9acde96e02c
SikongJueluo <selfconfusion@gmail.com>
2026-01-27 19:47:44 +0800

    Init python and env

---
+++ devenv.nix
@@ -0,0 +1,63 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: let
+  pkgs-nixgl =
+    (import inputs.nixpkgs {
+      system = pkgs.stdenv.hostPlatform.system;
+      config.allowUnfree = true;
+      overlays = [inputs.nixgl.overlay];
+    }).nixgl.override {
+      nvidiaVersionFile = "/proc/driver/nvidia/version";
+      nvidiaVersion = "580.126.09";
+    };
+in {
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = [
+    pkgs-nixgl.auto.nixGLNvidia
+  ];
+
+  # https://devenv.sh/languages/
+  # languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+    eval "$(micromamba shell hook --shell bash)"
+    micromamba activate ./.venv
+    export PATH=$PATH:/home/sikongjueluo/Projects/Mini-Nav
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
